3b53172fd1a27ace660d3d861f484bfd3a564060
pro2s <protasov.s.s@gmail.com>
2018-05-17 10:45:41 +0300

    update npm, prettier

--- src/actions/types.js
+++ src/actions/types.js
@@ -6,6 +6,4 @@ export const GET_SITE_DATA_RECEIVED = getReceivedAction(GET_SITE_DATA);
 export const GET_SECTION_DATA = 'GET_SECTION_DATA';
 export const GET_SECTION_DATA_RECEIVED = getReceivedAction(GET_SECTION_DATA);
 export const GET_PAGE_DESCRIPTION = 'GET_PAGE_DESCRIPTION';
-export const GET_PAGE_DESCRIPTION_RECEIVED = getReceivedAction(
-  GET_PAGE_DESCRIPTION
-);
+export const GET_PAGE_DESCRIPTION_RECEIVED = getReceivedAction(GET_PAGE_DESCRIPTION);
--- src/components/navigation/defaultLinks.js
+++ src/components/navigation/defaultLinks.js
@@ -1,18 +1,18 @@
-import React, { Fragment, Component } from 'react'
-import { NavItem } from 'react-bootstrap/lib'
-import { LinkContainer } from 'react-router-bootstrap'
+import React, { Fragment, Component } from 'react';
+import { NavItem } from 'react-bootstrap/lib';
+import { LinkContainer } from 'react-router-bootstrap';
 
 export default class DefaultLinks extends Component {
   render() {
     return (
-        <Fragment>
-            <LinkContainer to="/about-us" exact>
-                <NavItem>Пра нас</NavItem>
-            </LinkContainer>
-            <LinkContainer to="/counter" exact>
-                <NavItem>Лічыльнік</NavItem>
-            </LinkContainer>
-        </Fragment>
-    )
+      <Fragment>
+        <LinkContainer to="/about-us" exact>
+          <NavItem>Пра нас</NavItem>
+        </LinkContainer>
+        <LinkContainer to="/counter" exact>
+          <NavItem>Лічыльнік</NavItem>
+        </LinkContainer>
+      </Fragment>
+    );
   }
 }
